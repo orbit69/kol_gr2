a2b5470e69bc98d58d7e2e42dd31601551f46103
Maciej  Marzeta <4marzeta@stud233.ftj.agh.edu.pl>
2016-10-12 14:27:16 +0200

    lab1

--- kol_gr2.py
+++ kol_gr2.py
@@ -1,23 +1,44 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be named "kol_gr2"! 
-#
-#Delete these comments before commit!
-#Good luck.
+class Matrix:
+	def __init__(self, a, b, c, d):
+		self._matrix = [[0 for col in range(2)] for row in range(2)]
+		self._matrix[0][0] = a;
+		self._matrix[0][1] = b;
+		self._matrix[1][0] = c;
+		self._matrix[1][1] = d;
+
+	def add(self, mtrx):
+		tmp = Matrix(0, 0, 0, 0)
+		for i in range (0, 2):
+			for j in range (0, 2):
+				tmp._matrix[i][j] = self._matrix[i][j] + mtrx._matrix[i][j]
+		return tmp
+
+	def print_matrix(self, string):
+		print "Macierz ", string
+		print self._matrix[0]
+		print self._matrix[1]
+
+	def multiply(self, mtrx):
+		tmp = Matrix(0, 0, 0, 0)
+		for i in range (0, 2):
+			for j in range (0, 2):
+				for k in range (0, 2):
+					tmp._matrix[i][j] += self._matrix[k][j] * mtrx._matrix[i][k]
+		return tmp;
+
+	
+			
+
+
+matrix_1 = Matrix(4,5,6,7)
+matrix_1.print_matrix("matrix_1")
+
+matrix_2 = Matrix(2,2,2,1)
+matrix_2.print_matrix("matrix_2")
+
+matrix_3 = matrix_2.add(matrix_1)
+matrix_3.print_matrix("matrix_3")
+
+matrix_4 = matrix_1.multiply(matrix_2)
+matrix_4.print_matrix("matrix_4")
+
